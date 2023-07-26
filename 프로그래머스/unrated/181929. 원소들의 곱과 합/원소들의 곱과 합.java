edf1002307ebf741a25612ba9756087e879b27a8
yaangha <113163657+yaangha@users.noreply.github.com>
2023-07-26 10:00:51 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.02 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -8,10 +8,16 @@ class Solution {
             b *= num_list[i];
         }
         
+        //(1) if ~ else ~
+        /*
         if (b < a * a) {
             return 1;
         } else {
             return 0;
         }
+        */
+        
+        //(2) 삼항연산자
+        return (b < a * a) ? 1 : 0;
     }
 }
