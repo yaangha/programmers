91eb8b421ce533eff6bbad840c552962c2125402
yaangha <113163657+yaangha@users.noreply.github.com>
2023-07-28 12:44:47 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
@@ -4,17 +4,11 @@ class Solution {
         
         int[] answer = new int[len + 1];
         
-        if (num_list[len - 1] > num_list[len - 2]) {
-            for (int i = 0; i < len; i++) {
-                answer[i] = num_list[i];
-            }
-            answer[len] = num_list[len - 1] - num_list[len - 2];
-        } else {
-            for (int i = 0; i < len; i++) {
-                answer[i] = num_list[i];
-            }
-            answer[len] = num_list[len - 1] * 2;
+        for (int i = 0; i < len; i++) {
+            answer[i] = num_list[i];
         }
+        
+        answer[len] = (num_list[len - 1] > num_list[len - 2]) ? num_list[len - 1] - num_list[len - 2] : num_list[len - 1] * 2 ;
         
         return answer;
     }
