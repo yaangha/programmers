50a5c80b12cfcfe50ecab134855d820a312ad1c3
하은 <113163657+yaangha@users.noreply.github.com>
2023-08-17 23:52:57 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.04 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -2,9 +2,13 @@ class Solution {
     public int solution(String my_string, String is_prefix) {
         int answer = 0;
         
+        answer = my_string.startsWith(is_prefix) ? 1 : 0;
+        
+        /*
         if (my_string.length() >= is_prefix.length()) {
             answer = my_string.substring(0, is_prefix.length()).equals(is_prefix) ? 1 : 0;
         } 
+        */
         
         return answer;
     }
