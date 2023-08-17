b2f81f160b8ce45438cf268200136b745c535a63
하은 <113163657+yaangha@users.noreply.github.com>
2023-08-17 14:04:29 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -3,11 +3,11 @@ class Solution {
         int answer = price;
         
         if (price >= 500000) {
-            answer = (int) Math.floor(price * 0.8);
+            answer = (int) (price * 0.8);
         } else if (price >= 300000) {
-            answer = (int) Math.floor(price * 0.9);
+            answer = (int) (price * 0.9);
         } else if (price >= 100000) {
-            answer = (int) Math.floor(price * 0.95);
+            answer = (int) (price * 0.95);
         }
         
         return answer;
