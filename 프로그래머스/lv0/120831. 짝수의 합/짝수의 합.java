ce2adc6aa7fdbfb65a1ff5acaa02e5af43ceae77
하은 <113163657+yaangha@users.noreply.github.com>
2023-08-17 10:10:56 +0900

    [level 0] Title: 짝수의 합, Time: 1.03 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,19 @@
+import java.util.stream.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        /*
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                answer += i;
+            }
+        }
+        */
+        
+        answer = IntStream.rangeClosed(0, n).filter(value -> value % 2 == 0).sum();
+        
+        return answer;
+    }
+}
