6809891a0aea141367b2c2aa4fc9af73a8bd0ee4
하은 <113163657+yaangha@users.noreply.github.com>
2023-08-27 13:44:07 +0900

    [unrated] Title: 5명씩, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public String[] solution(String[] names) {
+        List<String> answer = new ArrayList<>();
+        
+        for (int i = 0; i < names.length; i++) {
+            if (i % 5 == 0) {
+                answer.add(names[i]);
+            }
+        }
+        
+        return answer.toArray(new String[0]);
+    }
+}
