9269dca3b90f6c4aab2afe29d35894196fa10d9e
하은 <113163657+yaangha@users.noreply.github.com>
2023-09-17 01:48:41 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.13 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(String s) {
+        String[] arr = s.split(" ");
+        
+        int answer = 0;
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals("Z")) {
+                answer -= Integer.parseInt(arr[i-1]);
+            } else {
+                answer += Integer.parseInt(arr[i]);
+            }
+        }
+        
+        return answer;
+    }
+}
